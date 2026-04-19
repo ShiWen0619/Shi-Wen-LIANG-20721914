e8003161458367707e8a3fab870bb882c8fe885b
Huo <Huo@TABLET-AQKRN544>
2026-04-19 15:26:50 +0800

    task1

--- Script_template.m
+++ Script_template.m
@@ -3,12 +3,43 @@
 
 
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
-
-% Insert answers here
+clear;
+a = arduino('COM5', 'Uno'); 
+for i = 1:10
+    writeDigitalPin(a, 'D4', 1); 
+    pause(0.5);                   
+    writeDigitalPin(a, 'D4', 0);                  
+end
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+% b)
+duration = 600;                    
+temps = zeros(1, duration+1);      
+V0C = 0.5;    
+TC  = 0.01;         
+disp('Data acquisition started');
+for t = 1:duration+1
+    Vout = readVoltage(a, 'A0');             
+    temps(t) = (Vout - V0C) / TC;             
+    if t < duration+1
+        pause(1);                         
+    end
+end
+disp('Data acquisition completed');
+min_temp = min(temps);
+max_temp = max(temps);
+avg_temp = mean(temps);
 
-% Insert answers here
+% c)
+figure('Position', [100 100 900 500]);
+plot((0:duration)/60, temps, 'b-', 'LineWidth', 2);  
+xlabel('Time (minutes)');
+ylabel('Temperature (°C)');
+title('Capsule Temperature Over 10 Minutes');
+grid on;
+saveas(gcf, 'capsule_temperature_plot.png');
+
+% d)
 
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
 
